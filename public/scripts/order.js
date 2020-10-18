bf394bb308b588166a0f1175c6c6152bc9ae2c18
vagrant <shadmerhi@gmail.com>
2020-10-18 21:11:20 +0000

    Items can be added to cart

--- public/scripts/order.js
+++ public/scripts/order.js
@@ -1,38 +1,54 @@
 $(document).ready(function() {
 
+
+
+
+
+  const createOrderItem = function(item) {
+    // console.log(item);
+    const $orderItem =
+    `<div class="container ml-auto align-middle">
+        <div class="row order-item text-center">
+          <div class="col-4 item-element">
+            <input type="number" id="item-quantity" name="item-quantity" min="0" max="20">
+          </div>
+          <div class="col-4 item-element">
+            <p>${item.name}</p>
+          </div>
+          <div class="col-4 item-element">
+            <button type="button" class="btn">
+              <i class="fas fa-trash"></i>
+            </button>
+          </div>
+        </div>
+      </div>`;
+    return $orderItem;
+  }
+
   const addButtons = $addButtons = $('.add-item-btn');
   console.log(addButtons);
 
-  const createOrderItem = function(item) {
-    console.log(item);
-    const $orderItem =
-    ``;
-  }
-
   // Adding click event listeners to all 'add item' buttons to add to order
-  $(addButtons).each(function(index) {
-    $(this).on('click', function () {
-      // $(this).parent().parent().parent().remove();
-      const name = $(this).parent().parent().children('.item-info').children('.item-name').html().trim();
-      const desc = $(this).parent().parent().children('.item-info').children('.item-description').html().trim();
-      const price = $(this).parent().parent().children('.item-info').children('.item-price').html().trim();
-      console.log('name', name);
-      console.log('desc', desc);
-      console.log('price', price);
+  // $(addButtons).each(function(event) {
+  $(addButtons).on('click', function (event) {
+    // $(this).parent().parent().parent().remove();
+    const name = $(this).parent().parent().children('.item-info').children('.item-name').html().trim();
+    const desc = $(this).parent().parent().children('.item-info').children('.item-description').html().trim();
+    const price = $(this).parent().parent().children('.item-info').children('.item-price').html().trim();
 
-      const item = {
-        name,
-        desc,
-        price
-      }
+    const item = {
+      name,
+      desc,
+      price
+    }
+    console.log(item);
 
-      const $orderItem = createOrderItem(item);
-
-    });
-
-
-
+    const $newItem = createOrderItem(item);
+    $('.order-item-container').prepend($newItem);
+    console.log($newItem);
+    console.log($('.order-item-container'));
   });
+  // });
 
 
   // console.log(addButtons[0].parents('.container').parents('.row'));
