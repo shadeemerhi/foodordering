be69a939d839f436f7a8813cbf36ccd0e5df4841
vagrant <ttjho@hotmail.com>
2020-10-18 20:17:13 +0000

    finished database seeds and shortened URLs for images

--- db/seeds/dishes.sql
+++ db/seeds/dishes.sql
@@ -1,5 +1,12 @@
 INSERT INTO dishes (name,description,price,category,photo_url)
 VALUES
-('Hamburger', 'really good', 500, 'Main', '🍔'),
-('Soup', 'really nice', 300, 'Appetizer', '🥣'),
-('Ice Cream', 'really good', 100, 'Desserts', '🍨');
+('Soup', 'Keeps you warm and healthy', 300, 'Appetizer', 'http://shorturl.at/dijs1'),
+('Poke Bowl', 'Mercury-free and dolphin friendly', 300, 'Appetizer','http://shorturl.at/bpxNX' ),
+('Calamari', 'Straight from the goddamned Kraken', 300, 'Appetizer','http://shorturl.at/hoV58' ),
+('Hamburger', 'Angus, Wagyu', 500,'Main','http://shorturl.at/gxyPT'),
+('Pizza', 'Even Mario would order this', 500, 'Main', 'http://shorturl.at/fxO36'),
+('Steak', 'So rare you can hearing it mooing', 500,'Main','http://shorturl.at/ouAEU'),
+('Ice Cream', 'Served by your local handlebar mustached hipster', 100, 'Desserts','http://shorturl.at/AFKNT' ),
+('Pie', 'Just like grandma', 100, 'Desserts', 'http://shorturl.at/iBG56'),
+('Cake', 'Decadent AF', 500, 'Desserts', 'http://shorturl.at/duP05');
+
