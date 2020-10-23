78e92058bfc0cff5a4e32e1aabd5f7aed57790e4
vagrant <shadmerhi@gmail.com>
2020-10-23 01:24:46 +0000

    Added redirect for '/'

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.use('/checkout',twilioRoutes());
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 app.get('/', (req, res) => {
-  res.redirect('/order');
+  res.redirect('/users/order');
 });
 
 
